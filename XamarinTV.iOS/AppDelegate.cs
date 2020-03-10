ff6b6e04a73b6f9fdb7c50f343557b76db9b5a77
Shane Neuville <shneuvil@microsoft.com>
2020-03-10 16:11:00 -0600

    ios implementation

---
+++ XamarinTV.iOS/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using FFImageLoading.Forms.Platform;
+using Foundation;
+using Sharpnado.Presentation.Forms.iOS;
+using UIKit;
+
+namespace XamarinTV.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            CachedImageRenderer.Init();
+            SharpnadoInitializer.Initialize();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
+
